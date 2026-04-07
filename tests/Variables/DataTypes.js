715cac8cd6e9939b0195672691ee7aece8ffa06b
akshaykamble-works <Akshay.kamble.work@gmail.com>
2026-04-07 18:28:27 +0530

    Add DataTypes.js file

---
+++ tests/Variables/DataTypes.js
@@ -0,0 +1,19 @@
+// Number data type
+let age = 30;
+console.log(age); // Output: 30
+
+// String data type
+let name = "John";
+console.log(name); // Output: John
+
+// Boolean data type
+let isStudent = true;
+console.log(isStudent); // Output: true
+
+// Undefined data type
+let address;
+console.log(address); // Output: undefined
+
+// Null data type
+let phoneNumber = null;
+console.log(phoneNumber); // Output: null
