9d1b47d11d7a8ea4cd1f2efe9989c4b52909f344
akshaykamble-works <Akshay.kamble.work@gmail.com>
2026-04-12 20:56:47 +0530

    Add Operator.js file

---
+++ tests/Variables/Operator.js
@@ -0,0 +1,11 @@
+let a=100;
+let b=20;
+
+console.log("Arithmetic Operators:");
+console.log("Addition: " + (a + b));
+console.log("Subtraction: " + (a - b));
+console.log("Multiplication: " + (a * b));
+console.log("Division: " + (a / b));
+console.log("Modulus: " + (a % b));
+
+
