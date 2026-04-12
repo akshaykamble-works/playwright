5012c1ec8ed788082c7cf0b5430d5db541ba6380
akshaykamble-works <Akshay.kamble.work@gmail.com>
2026-04-12 21:03:11 +0530

    Add AssignmentOperator.js file

---
+++ tests/Variables/AssignmentOperator.js
@@ -0,0 +1,20 @@
+// Assignment Operator (=) in JavaScript
+// The assignment operator is used to assign a value to a variable. It takes the value on the right and assigns it to the variable on the left.
+
+let a = 10; // Assigning the value 10 to variable a
+console.log(a); // Output: 10
+
+let b = "Hello"; // Assigning the string "Hello" to variable b
+console.log(b); // Output: Hello
+
+// We can also assign the value of one variable to another
+let c = a; // Assigning the value of variable a (which is 10) to variable c
+console.log(c); // Output: 10
+
+// The assignment operator can also be used with expressions
+let d = a + 5; // Assigning the result of the expression (a + 5) to variable d
+console.log(d); // Output: 15
+
+// We can also use compound assignment operators like +=, -=, *=, /=, etc.
+a += 5; // This is equivalent to a = a + 5
+console.log(a); // Output: 15
