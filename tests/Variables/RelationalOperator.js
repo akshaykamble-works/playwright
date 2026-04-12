3afddfe84b682874b176de5e1e04b454c216140f
akshaykamble-works <Akshay.kamble.work@gmail.com>
2026-04-12 21:13:47 +0530

    Update RelationalOperator.js examples

--- tests/Variables/RelationalOperator.js
+++ tests/Variables/RelationalOperator.js
@@ -1,11 +1,23 @@
-// Relations Operators
-let a = 10;
-let b = 20;
+// Relational Operators
+const a = 10;
+const b = 20;
 
 console.log("Relational Operators:");
-console.log("Equal to: " + (a == b));
-console.log("Not equal to: " + (a != b));
-console.log("Greater than: " + (a > b));
-console.log("Less than: " + (a < b));
-console.log("Greater than or equal to: " + (a >= b));
-console.log("Less than or equal to: " + (a <= b));      
+console.log("a == b: " + (a == b));
+console.log("a != b: " + (a != b));
+console.log("a > b: " + (a > b));
+console.log("a < b: " + (a < b));
+console.log("a >= b: " + (a >= b));
+console.log("a <= b: " + (a <= b));
+console.log("a === b: " + (a === b));
+console.log("a !== b: " + (a !== b));
+
+// Logical Operators
+const x = true;
+const y = false;
+
+console.log("Logical Operators:");
+console.log("x && y: " + (x && y));
+console.log("x || y: " + (x || y));
+console.log("!x: " + (!x));
+
