b981d439b06a94f21969e7fdf07d97a1ea9470a5
akshaykamble-works <Akshay.kamble.work@gmail.com>
2026-04-12 20:05:40 +0530

    Add VariableBoolean.js file

---
+++ tests/Variables/VariableBoolean.js
@@ -0,0 +1,18 @@
+let a=10;
+let b=20;
+let c=a>b;
+console.log("value of a "+a+ " and value of b "+b+ " "+c); // Output: false
+
+let d=a<b;
+console.log("value of a "+a+ " and value of b "+b+ " "+d); // Output: true
+
+let e=a==b;
+console.log("value of a "+a+ " and value of b "+b+ ""+e); // Output: false
+
+/*
+Key Points:
+- Only two values: true or false
+- Result of comparisons (5 > 3 → true)
+- Falsy values: false, 0, "", null, undefined, NaN
+- Truthy values: Everything else
+*/
