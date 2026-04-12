455e23d5335b5ae80eb019adfa9f18a261b99d82
akshaykamble-works <Akshay.kamble.work@gmail.com>
2026-04-12 21:06:50 +0530

    Add RelationalOperator.js file

---
+++ tests/Variables/RelationalOperator.js
@@ -0,0 +1,11 @@
+// Relations Operators
+let a = 10;
+let b = 20;
+
+console.log("Relational Operators:");
+console.log("Equal to: " + (a == b));
+console.log("Not equal to: " + (a != b));
+console.log("Greater than: " + (a > b));
+console.log("Less than: " + (a < b));
+console.log("Greater than or equal to: " + (a >= b));
+console.log("Less than or equal to: " + (a <= b));      
