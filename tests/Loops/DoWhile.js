a0b677dfe02732ad20e8e35f7721cd1de41e3f2d
akshaykamble-works <Akshay.kamble.work@gmail.com>
2026-04-13 22:08:22 +0530

    Add DoWhile loop test

---
+++ tests/Loops/DoWhile.js
@@ -0,0 +1,13 @@
+// Do while Loop
+// Definition: A control flow statement that executes a block of code at least once, and then repeatedly executes it as long as a specified condition is true.
+// Syntax:
+// do {
+//   // code block to be executed
+// } while (condition);
+
+// Example:
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
