57b9c5002dee2e637cd663dd0e350d1b22e5c16f
akshaykamble-works <Akshay.kamble.work@gmail.com>
2026-04-13 22:20:53 +0530

    Update FoorLoop test

--- tests/Loops/FoorLoop.js
+++ tests/Loops/FoorLoop.js
@@ -39,3 +39,12 @@ console.log("--------------------");
 for(let i=10;i>0;i--){
     console.log(i);
 }
+// Example 6: Nested for loop to print a pattern
+console.log("--------------------");
+for(let i=1;i<=5;i++){
+    let pattern = "";
+    for(let j=1;j<=i;j++){
+        pattern += "* ";
+    }
+    console.log(pattern);
+}
